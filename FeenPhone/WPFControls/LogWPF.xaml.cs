f9d75733ed72af0e4a491a6075e871b86618021d
Derrick- <derrick@alienseed.com>
2014-08-06 16:12:47 +0000

    Moved console log to new control
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/FeenPhone@2986 4bf54c5a-0cb7-ff49-8398-c775ed83c514

---
+++ FeenPhone/WPFControls/LogWPF.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FeenPhone.WPFControls
+{
+    /// <summary>
+    /// Interaction logic for LogWPF.xaml
+    /// </summary>
+    public partial class LogWPF : UserControl
+    {
+        public LogWPF()
+        {
+            InitializeComponent();
+
+            var console = new ConsoleWriter();
+            console.OnText += console_OnText;
+            Console.SetOut(console);
+
+        }
+        void console_OnText(object sender, ConsoleWriter.OnTextEventArgs e)
+        {
+            Dispatcher.BeginInvoke(new Action<string>(AddToLog), e.Text);
+        }
+
+        void AddToLog(string text)
+        {
+            log.Text += text;
+        }
+
+        class ConsoleWriter : TextWriter
+        {
+
+            public class OnTextEventArgs : EventArgs
+            {
+                public string Text { get; set; }
+                public OnTextEventArgs(string text)
+                {
+                    Text = text;
+                }
+            }
+
+            public event EventHandler<OnTextEventArgs> OnText;
+
+            public ConsoleWriter()
+            {
+            }
+
+            public override Encoding Encoding
+            {
+                get { return Console.OutputEncoding; }
+            }
+
+            public override void Write(char[] buffer, int index, int count)
+            {
+                if (OnText != null)
+                {
+                    string text = new String(buffer, index, count);
+                    OnText(this, new OnTextEventArgs(text));
+                }
+            }
+
+        }
+
+
+    }
+}
