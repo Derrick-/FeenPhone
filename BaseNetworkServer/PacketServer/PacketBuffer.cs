32c88ba100cf3a012635ef1549274584a6a0a5ce
Derrick- <derrick@alienseed.com>
2015-01-06 20:56:44 +0000

    PacketBuffer uses Byte[] instead of Queue<byte>: Prep for reuse of packet buffer objects
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/FeenPhone@3632 4bf54c5a-0cb7-ff49-8398-c775ed83c514

--- BaseNetworkServer/PacketServer/PacketBuffer.cs
+++ BaseNetworkServer/PacketServer/PacketBuffer.cs
@@ -7,36 +7,45 @@ namespace Alienseed.BaseNetworkServer.PacketServer
 {
     public class PacketBuffer : IDisposable
     {
-        private Queue<byte> Bytes;
+        private int BytesLength = 0;
+        private byte[] Bytes;
 
+        protected virtual int DefaultSize { get { return 1024; } }
         protected virtual int MaxLength { get { return int.MaxValue; } }
-
-        protected int CurrentLength
-        {
-            get { return Bytes.Count(); }
-        }
 
         public PacketBuffer(int? initialSize = null)
         {
             if (initialSize.HasValue)
-                Bytes = new Queue<byte>(initialSize.Value);
+                Bytes = new byte[initialSize.Value];
             else
-                Bytes = new Queue<byte>();
+                Bytes = new byte[DefaultSize];
         }
 
         byte[] data = null;
         private void Invalidate() { data = null; }
         public byte[] GetData()
         {
-            return data ?? (data = Bytes.ToArray());
+            return data ?? (data = Bytes.Take(BytesLength).ToArray());
         }
 
         public void Write(byte value)
         {
-            if (CurrentLength + 1 > MaxLength)
-                throw new ArgumentException("Packet overflow", "value");
-            Bytes.Enqueue(value);
+            EnsureCapacity(BytesLength + 1);
+            Bytes[BytesLength++] = value;
             Invalidate();
+        }
+
+        private void EnsureCapacity(int requiredLength)
+        {
+            if (requiredLength > MaxLength)
+                throw new ArgumentException("Exceeded max buffer length", "requiredLength");
+            if (Bytes.Length < requiredLength)
+            {
+                int newlength = Bytes.Length < MaxLength / 2 ? requiredLength * 2 : MaxLength;
+                byte[] newBytes = new byte[newlength];
+                Buffer.BlockCopy(Bytes, 0, newBytes, 0, BytesLength);
+                Bytes = newBytes;
+            }
         }
 
         public void Write(bool value)
@@ -46,34 +55,24 @@ namespace Alienseed.BaseNetworkServer.PacketServer
 
         public void Write(ushort value)
         {
-            Write((byte)(value >> 8));
-            Write((byte)value);
+            EnsureCapacity(BytesLength + 2);
+            Bytes[BytesLength++] = (byte)(value >> 8);
+            Bytes[BytesLength++] = (byte)value;
         }
 
-        public void Write(byte[] data, int? length = null)
+        public void Write(byte[] data, int? optLength = null)
         {
-            if (length == null)
+            int length;
+            if (optLength == null)
                 length = data.Length;
             else
-                length = Math.Min(length.Value, data.Length);
+                length = Math.Min(optLength.Value, data.Length);
 
-            if (CurrentLength + length > MaxLength)
-                throw new ArgumentException("Packet overflow", "data");
+            int newLength = BytesLength + length;
 
-            IEnumerable<byte> bytes = data;
-            if (length < data.Length)
-                bytes = bytes.Take(length.Value);
-
-            if (length > 100)
-            {
-                Bytes = new Queue<byte>(Bytes.Concat(bytes));
-                Invalidate();
-            }
-            else
-            {
-                foreach (byte value in bytes)
-                    Write(value);
-            }
+            EnsureCapacity(newLength);
+            Buffer.BlockCopy(data, 0, Bytes, BytesLength, length);
+            BytesLength = newLength;
         }
 
         public void Dispose()
