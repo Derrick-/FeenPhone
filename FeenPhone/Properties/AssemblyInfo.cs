dd7957b0ecd28946c228b7563981550599c1146b
Derrick- <derrick@alienseed.com>
2015-01-08 04:13:40 +0000

    Added Wix installer project: Requires toolset from http://wixtoolset.org/ Working Installer, but no dialogs. Updated assembly year
    git-svn-id: https://svn.alienseed.com:8443/svn/alienseed/trunk/FeenPhone@3649 4bf54c5a-0cb7-ff49-8398-c775ed83c514

--- FeenPhone/Properties/AssemblyInfo.cs
+++ FeenPhone/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Beastlick Internet Policy Commission")]
 [assembly: AssemblyProduct("FeenPhone")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
